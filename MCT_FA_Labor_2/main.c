06eb21273b9021a4e787885d2d1118990ff9ea2f
Jonas Heubuch <jonas.heubuch@outlook.com>
2021-11-16 10:06:35 +0100

    labor 2

--- MCT_FA_Labor_2/main.c
+++ MCT_FA_Labor_2/main.c
@@ -7,7 +7,6 @@
 
 #include <msp430.h>
 
-
 void DoSomething(void){
   __delay_cycles(250000UL);
 }
@@ -19,20 +18,47 @@ void sysInit(void) {
     CSCTL0_H = 0;                       //re-lock CS-registers
 }
 
+//#pragma vector = PORT5_VECTOR
+//__interrupt void P5_ISRb(void) {
+//
+//    switch (P5IV) {
+//        // S2
+//        case 0x0C:
+//            P1OUT &= ~BIT0;
+//            break;
+//        // S1
+//        case 0x0E:
+//            P1OUT |= BIT0;
+//            break;
+//    }
+//
+//}
+//__interrupt void P5_ISR(void) {
+//    if (P5IFG & BIT6) {
+//        P1OUT |= BIT0;
+////        P5IFG &= ~BIT6;
+//    }
+//
+//    if (P5IFG & BIT5) {
+//        P1OUT &= ~BIT0;
+////        P5IFG &= ~BIT5;
+//    }
+//}
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;           //CODE 1
     sysInit();
                                         //CODE 2
-    P5DIR = ~(BIT5 | BIT6);
-    P5REN = (BIT5 | BIT6);
-    P5OUT = (BIT5 | BIT6);
+    P5DIR = ~(BIT5 + BIT6);
+    P5REN = (BIT5 + BIT6);
+    P5OUT = (BIT5 + BIT6);
 
                                         //CODE 3
+    P5IES = (BIT5 + BIT6);
 
                                         //CODE 4
-    P1DIR = ~0;
+    P1DIR = 0xFF;
     P1OUT = 0;
 
     PM5CTL0 &= ~LOCKLPM5;               //Enable digital IO
@@ -40,15 +66,26 @@ int main(void)
     P5IFG = 0;                          //Clear all pending i.e. old interrupt flags
 
                                         //CODE 5
+    P5IE = (BIT5 + BIT6);
 
                                         //CODE 6
+    __enable_interrupt();
+
     while(1){
                                         //CODE 7
+        DoSomething();
 
                                         //CODE 8
+//        if (!(P5IN & BIT6)) // S1 pressed
+//            P1OUT |= BIT0;
+//
+//        if (!(P5IN & BIT5))
+//            P1OUT &= ~BIT0;
 
 
                                         //CODE 9
+
+
 
                                         //CODE 10
     };
