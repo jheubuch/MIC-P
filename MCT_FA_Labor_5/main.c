9a0315c42dac79add4fff3c034d46114bbf72577
Felix Jaensch <felix-jaensch@web.de>
2021-12-14 12:31:57 +0100

    Added files for exercise 5

---
+++ MCT_FA_Labor_5/main.c
@@ -0,0 +1,32 @@
+#include <msp430.h> 
+#include <stdint.h>
+
+/*
+=====================================================
+  Autor:                Name
+  Erstellungsdatum:     dd.mm.yyyy
+  Beschreibung:         5. Labor zur Vorlesung Mikrocontroller
+  Datei:                main.c - Timer_A Module - Periodendauer und Frequenzmessung
+=====================================================
+*/
+
+
+
+
+int main(void)
+{
+
+    WDTCTL = WDTPW | WDTHOLD;
+                                   //Initialize I/O ports
+                                   //Enable digital IO
+    while(1){
+
+    }
+}
+
+
+
+
+
+
+
